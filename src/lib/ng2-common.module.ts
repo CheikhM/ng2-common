7bf25b20c64810f313397f1cc1271d2cb52c5fe8
CHEIKH EL MOCTAR Mohamed Yehdhih <elmoctar.yehdhih@gmail.com>
2020-10-11 23:54:08 +0200

    initalize confimation modal code

--- src/lib/ng2-common.module.ts
+++ src/lib/ng2-common.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToggleDirective } from './directives/nc-toggle.directive';
+import { MatDialogModule } from "@angular/material/dialog";
+import { ConfirmationBoxModal } from './components/confirm-box/confirm-modal.component';
+import { ConfirmBox } from './components/confirm-box/confirm-box-component';
 @NgModule({
   declarations: [
-    ToggleDirective
+    ToggleDirective,
+    ConfirmationBoxModal,
+    ConfirmBox,
   ],
   imports: [
     BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    ConfirmationBoxModal
   ],
   exports: [
     ToggleDirective,
+    ConfirmBox,
   ]
 })
 export class Ng2CommnonModule { }
